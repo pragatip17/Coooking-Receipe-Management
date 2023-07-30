75bc388a139fd0213752f41d32fee60f857cb0f0
pragatip17 <86849698+pragatip17@users.noreply.github.com>
2023-07-31 02:49:38 +0530

    Initial commit

---
+++ food website/index.php
@@ -0,0 +1,271 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Recipe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <img src="./images/logo.jpg" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Recipe</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<section class="main">
+    <div class="container py-5">
+        <div class="row py-5">
+            <div class="col-lg-7 pt-5 text-center">
+                <h1 class="pt-5">Tasty and easy food recipes!!</h1>
+                <button class="btn1">Recipes</button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="new">
+    <div class="container py-5">
+        <div class="row pt-5">
+            <div class="col-lg-7 m-auto">
+                <div class="row text-center">
+                    <div class="col-lg-4">
+                        <img src="./images/snacks.png" class="img-luid" alt="">
+                        <h6>Snacks</h6>
+                    </div>
+                    <div class="col-lg-4">
+                      <img src="./images/meal.png" class="img-luid" alt="">
+                      <h6>Meal</h6>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="./images/Dessert.png" class="img-luid" alt="">
+                        <h6>Desserts</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="product">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-5 m-auto text-center">
+                <h1><u>Breakfast</u></h1>
+                <h5 style="color: aquamarine;">Start your day with a meal full of happiness</h5>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-lg-3 text-center">
+            <a href="./sop.html">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/sop.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Spring onion paratha</h6>
+            </a>
+            </div>
+            
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/sandwich.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Sandwich</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/upma.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Upma</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/idli.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Idly</h6>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="product">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-5 m-auto text-center">
+                <h1><u>Lunch</u></h1>
+                <h5 style="color: aquamarine;">Food that fills you</h5>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-lg-3 text-center">
+            <a href="./sop.html">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/rice.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Veg fried rice</h6>
+            </a>
+            </div>
+            
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/rasam.jpeg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Tomato rasam</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/rajma.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Masala Rajma</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/curry.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Green peas curry</h6>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="product">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-5 m-auto text-center">
+                <h1><u>Dinner</u></h1>
+                <h5 style="color: aquamarine;">Food that fills you</h5>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-lg-3 text-center">
+            <a href="./sop.html">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/tomatorice.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Tomato rice</h6>
+            </a>
+            </div>
+            
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/tmc.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Mac & Cheese</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/tamrindrice.jpeg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Tamrind rice</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/curdrice.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Curd rice</h6>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="product">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-5 m-auto text-center">
+                <h1><u>Appetizer</u></h1>
+                <h5 style="color: aquamarine;">Food that makes you crave for an extra bite</h5>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-lg-3 text-center">
+            <a href="./sop.html">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/pavbhaji.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Pav bhaji</h6>
+            </a>
+            </div>
+            
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/manchurian.jpeg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Gobi manchurian</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/masalapuri.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Masala puri</h6>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                    <div class="card-body">
+                        <img src="./images/tikki.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <h6>Aloo tikki chole chat</h6>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
